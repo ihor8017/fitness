5bd5a39d3d9ae420a29547448f887b4aef0e568f
ihor8017 <109109118+ihor8017@users.noreply.github.com>
2023-07-22 16:05:52 -0500

    script update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-import playVideo from"./video.js";import{showPlan}from"./show-plans.js";import swiper from"./vendor/slider.js";import swiperReviews from"./vendor/slider.js";import{initTabs}from"./vendor/init-tabs.js";import{initAccordions}from"./vendor/init-accordion.js";import formValidate from"./validate.js";playVideo(),showPlan(),formValidate(),window.addEventListener("DOMContentLoaded",(()=>{window.addEventListener("load",(()=>{initTabs()}))})),window.addEventListener("DOMContentLoaded",(()=>{window.addEventListener("load",(()=>{initAccordions()}))}));
+/* в этот файл добавляет скрипты*/
+import playVideo from "./video.js";
+import { showPlan } from "./show-plans.js";
+import swiper from "./slider.js";
+import swiperReviews from "./slider.js";
+import {initTabs} from "./vendor/init-tabs.js";
+import {initAccordions} from './vendor/init-accordion.js';
+import formValidate from "./validate.js";
+
+
+playVideo();
+showPlan();
+formValidate();
+swiper;
+swiperReviews;
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initTabs();
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initAccordions();
+  });
+});
