ed3e02256c459953677ea5ca1fd30a7989fcd870
ihor8017 <109109118+ihor8017@users.noreply.github.com>
2023-07-28 17:06:50 -0500

    imask

---
+++ js/mask.js
@@ -0,0 +1,1 @@
+import IMask from"./vendor/imask.js";let phoneInput=document.querySelector(".form-lesson__phone input");const phoneMask=new IMask(phoneInput,{mask:"0-000-000-00-00"});export default phoneMask;
