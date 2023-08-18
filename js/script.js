3dc75a68bb960a13b4b4c934706e718de8a436a6
ihor8017 <109109118+ihor8017@users.noreply.github.com>
2023-08-17 23:07:24 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-import toggleNavigation from"./menu.js";import swiperHero from"./hero-slider.js";import playVideo from"./video.js";toggleNavigation(),swiperHero(),playVideo();
+import toggleNavigation from"./menu.js";import swiperHero from"./hero-slider.js";import playVideo from"./video.js";import upcomingSlider from"./upcoming-tours-slider.js";toggleNavigation(),swiperHero(),playVideo(),upcomingSlider();
